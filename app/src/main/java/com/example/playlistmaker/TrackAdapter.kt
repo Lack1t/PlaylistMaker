4b0ce484f271d51d90af04f59a63884abb617d23
Lack1t <lackit27@yandex.ru>
2023-11-08 13:50:58 +0300

    "Спринт 14"

--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.playlistmaker
 
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,8 +9,11 @@ import androidx.recyclerview.widget.RecyclerView
 class TrackAdapter(
     private var trackList: List<Track>,
     private val searchHistory: SearchHistory,
-    private val itemClickListener: (Track) -> Unit // Callback for item clicks
+    private val itemClickListener: (Track) -> Unit
 ) : RecyclerView.Adapter<TrackViewHolder>() {
+
+    private val clickHandler = Handler(Looper.getMainLooper())
+    private val debounceDelay: Long = 300
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.search_view, parent, false)
@@ -20,30 +25,38 @@ class TrackAdapter(
         holder.bind(track)
 
         holder.itemView.setOnClickListener {
-            itemClickListener(track) // Invoke the item click callback
-            val history = searchHistory.loadSearchHistory().toMutableList()
-
-            if (history.size >= searchHistory.maxHistorySize) {
-                history.removeAt(history.size - 1)
-            }
-
-            val existingIndex = history.indexOfFirst { it.trackId == track.trackId }
-            if (existingIndex != -1) {
-                history.removeAt(existingIndex)
-            }
-
-            history.add(0, track)
-
-            searchHistory.saveSearchHistory(history)
-
+            clickHandler.removeCallbacksAndMessages(null)
+            clickHandler.postDelayed({
+                itemClickListener(track)
+                addToSearchHistory(track)
+            }, debounceDelay)
         }
     }
 
-    override fun getItemCount() = trackList.size
+    override fun getItemCount(): Int {
+        return trackList.size
+    }
 
     fun updateData(newTrackList: List<Track>) {
         trackList = newTrackList
         notifyDataSetChanged()
     }
+
+    private fun addToSearchHistory(track: Track) {
+        val history = searchHistory.loadSearchHistory().toMutableList()
+
+        if (history.size >= searchHistory.maxHistorySize) {
+            history.removeAt(history.size - 1)
+        }
+
+        val existingIndex = history.indexOfFirst { it.trackId == track.trackId }
+        if (existingIndex != -1) {
+            history.removeAt(existingIndex)
+        }
+
+        history.add(0, track)
+        searchHistory.saveSearchHistory(history)
+    }
 }
+
 
